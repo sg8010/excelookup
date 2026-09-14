caa9c780fef99211b540de23b807dccb5988e28e
sg8010 <sg8011@outlook.com>
2026-09-14 16:38:08 +0800

    修复 Linux 文件选择器鼠标光标

--- src/file_dialog.rs
+++ src/file_dialog.rs
@@ -598,7 +598,8 @@ impl FileDialog {
                                 )
                                 // 标签默认只响应 hover，会挡住表格行的点击；把点击能力
                                 // 加回标签本身，文件名文字和行内空白区域行为保持一致。
-                                .interact(egui::Sense::click());
+                                .interact(egui::Sense::click())
+                                .on_hover_cursor(egui::CursorIcon::PointingHand);
                             if response.double_clicked() {
                                 row_intent = Some(RowIntent::Activate(entry.path.clone(), is_dir));
                             } else if response.clicked() {
